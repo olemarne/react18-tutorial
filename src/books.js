942fa7568812eaa71c16154ae30858cb5fbed5fc
olemarne <133375019+olemarne@users.noreply.github.com>
2023-11-23 16:08:07 +0100

    la til flere filer i src

---
+++ src/books.js
@@ -0,0 +1,14 @@
+export const books = [
+  {
+    author: 'Matthew Perry',
+    title: 'Friends, Lovers and the Big Terrible Thing',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81tdvyI0MeL._AC_UL600_SR600,400_.jpg',
+    id: 1,
+  },
+  {
+    author: 'Rebecca Yarros',
+    title: 'Fourth Wing',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/91n7p-j5aqL._AC_UL600_SR600,400_.jpg',
+    id: 2,
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,24 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import Book from './Book';
+import { books } from './books';
 import './index.css';
-
-const books = [
-  {
-    author: 'Matthew Perry',
-    title: 'Friends, Lovers and the Big Terrible Thing',
-    img: 'https://images-na.ssl-images-amazon.com/images/I/81tdvyI0MeL._AC_UL600_SR600,400_.jpg',
-    id: 1,
-  },
-  {
-    author: 'Rebecca Yarros',
-    title: 'Fourth Wing',
-    img: 'https://images-na.ssl-images-amazon.com/images/I/91n7p-j5aqL._AC_UL600_SR600,400_.jpg',
-    id: 2,
-  },
-];
-
-
 
 function BookList() {
   return (
@@ -27,21 +12,8 @@ function BookList() {
         return <Book {...book} key={book.id} />;
       })}
     </section>
-  )
-}
-
-const Book = (props) => {
-  const { img, title, author } = props;
-  console.log(props);
-  return (
-    <article className='book'>
-      <img src={img}
-        alt={title} />
-      <h2>{title}</h2>
-      <h4>{author}</h4>
-    </article>
   );
-};
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
